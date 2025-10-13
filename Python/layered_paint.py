224762555524a85e287aefcac53f6e76a4ea2350
Thomas H. <vhhorne2@gmail.com>
2025-10-13 19:39:13 -0400

    add layered paint algorithm

---
+++ Python/layered_paint.py
@@ -0,0 +1,65 @@
+# Simple Linear Iterative Clustering (SLIC)
+# https://darshita1405.medium.com/superpixels-and-slic-6b2d8a6e4f08
+# https://www.epfl.ch/labs/ivrl/research/slic-superpixels/
+
+from PIL import Image, ImageDraw, ImageFilter
+import math
+import numpy as np
+from typing import List, Tuple, NamedTuple
+
+BLUR_FACTOR = 0.5
+
+class LayeredPaintImage:
+    image: Image.Image
+    draw: ImageDraw.ImageDraw
+    brush_sizes: List[int]
+
+    def __init__(self, image: Image.Image):
+        self.image = image
+        self.pixels = image.load()
+        self.num_pixels = image.size[0] * image.size[1]
+
+    def paint(self):
+        canvas = Image.new("RGBA", self.image.size, (255, 255, 255, 255))
+        draw = ImageDraw.Draw(canvas)
+        w, h = self.image.size 
+        threshold = 4000
+        
+        refresh = True
+
+        initial_brush_size = self.brush_sizes[0]
+        for y in range(0, h, initial_brush_size):
+            for x in range(0, w, initial_brush_size):
+                M = np.array(self.image.crop((x, y, x + initial_brush_size, y + initial_brush_size)))
+                draw.rectangle((x, y, x + initial_brush_size, y + initial_brush_size), fill=tuple(M.mean(axis=(0, 1)).astype(int)))
+
+        for brush_size in self.brush_sizes:
+
+            blurred = self.image.copy().filter(ImageFilter.GaussianBlur(radius=brush_size * BLUR_FACTOR))
+            blurred_array = np.array(blurred)
+
+            grid = brush_size
+            for y in range(0, h, brush_size):
+                for x in range(0, w, brush_size):
+                    M = blurred_array[y:y+grid, x:x+grid]
+                    cropped_canvas = np.array(canvas.crop((x, y, x + brush_size, y + brush_size)))
+                    # Ensure both arrays have the same shape
+                    if cropped_canvas.shape != M.shape:
+                        min_shape = tuple(map(min, cropped_canvas.shape, M.shape))
+                        cropped_canvas = cropped_canvas[:min_shape[0], :min_shape[1], :min_shape[2]]
+                        M = M[:min_shape[0], :min_shape[1], :min_shape[2]]
+                    difference_array = np.linalg.norm(cropped_canvas[..., :3] - M[..., :3], axis=2)
+                    summed_difference = np.sum(difference_array, axis=(0, 1))
+                    print(summed_difference)
+                    if refresh or (summed_difference > threshold):
+                        x1, y1 = np.unravel_index(np.argmax(difference_array), difference_array.shape)
+                        draw_box = (x + x1 - brush_size // 2, y + y1 - brush_size // 2, x + x1 + brush_size // 2, y + y1 + brush_size // 2)
+                        draw.ellipse(draw_box, fill=tuple(M.mean(axis=(0, 1)).astype(int)), outline=tuple(M.mean(axis=(0, 1)).astype(int)))
+            refresh = False
+        return canvas
+
+        
+    def set_brush_sizes(self, *args):
+        args = sorted(args)
+        args.reverse()
+        self.brush_sizes = list(args)
--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,20 @@
 from PIL import Image, ImageDraw
 
 from slic import SLICImage
+from layered_paint import LayeredPaintImage
 
 filename = "Inputs/peppers.png"
 outputname = "Outputs/peppers.png"
 with Image.open(filename) as img:
     img.load()
     draw = ImageDraw.Draw(img)
-    slic_image = SLICImage(img, draw)
-    labels, centers = slic_image.slic(superpixel_ratio=36, num_iterations=20, compactness=13)
-    
-    slic_image.draw_splots(labels)
-    img.show()
+    # slic_image = SLICImage(img, draw)
+    # labels, centers = slic_image.slic(superpixel_ratio=36, num_iterations=20, compactness=13)
+    # slic_image.draw_splots(labels)
+    layered_paint = LayeredPaintImage(img)
+    layered_paint.set_brush_sizes(5, 10, 20, 40)
+    #layered_paint.set_brush_sizes(80)
+    painted_image = layered_paint.paint()
+    painted_image.show()
 
-img.save(outputname)
+painted_image.save(outputname)
