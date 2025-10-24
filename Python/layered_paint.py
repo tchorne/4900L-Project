bf34f73724dcb0aa2843b9751e48f578e4dc4ac3
Thomas H. <vhhorne2@gmail.com>
2025-10-24 14:36:26 -0400

    implement brush textures

--- Python/layered_paint.py
+++ Python/layered_paint.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw, ImageFilter
 import math
 import numpy as np
 from typing import List, Tuple, NamedTuple
+from functools import lru_cache
 
 BLUR_FACTOR = 0.5
 
@@ -18,6 +19,7 @@ class LayeredPaintImage:
         self.image = image
         self.pixels = image.load()
         self.num_pixels = image.size[0] * image.size[1]
+        self.brush = Brush(Image.open("Brushes/paint1.png"))
 
     def paint(self):
         canvas = Image.new("RGBA", self.image.size, (255, 255, 255, 255))
@@ -39,6 +41,8 @@ class LayeredPaintImage:
             blurred_array = np.array(blurred)
 
             grid = brush_size
+            brush_mask = self.brush.get_bitmap_for_size(brush_size)
+
             for y in range(0, h, brush_size):
                 for x in range(0, w, brush_size):
                     M = blurred_array[y:y+grid, x:x+grid]
@@ -50,16 +54,36 @@ class LayeredPaintImage:
                         M = M[:min_shape[0], :min_shape[1], :min_shape[2]]
                     difference_array = np.linalg.norm(cropped_canvas[..., :3] - M[..., :3], axis=2)
                     summed_difference = np.sum(difference_array, axis=(0, 1))
-                    print(summed_difference)
+
                     if refresh or (summed_difference > threshold):
                         x1, y1 = np.unravel_index(np.argmax(difference_array), difference_array.shape)
                         draw_box = (x + x1 - brush_size // 2, y + y1 - brush_size // 2, x + x1 + brush_size // 2, y + y1 + brush_size // 2)
-                        draw.ellipse(draw_box, fill=tuple(M.mean(axis=(0, 1)).astype(int)), outline=tuple(M.mean(axis=(0, 1)).astype(int)))
+
+                        draw_color = tuple(M.mean(axis=(0, 1)).astype(int))
+                        #draw.ellipse(draw_box, fill=draw_color, outline=draw_color)
+                        canvas.paste(draw_color, box=draw_box, mask=brush_mask)
             refresh = False
         return canvas
 
         
     def set_brush_sizes(self, *args):
+        
+        for (arg) in args:
+            if arg % 2 == 1:
+                raise ValueError("Brush sizes must be even numbers.")
+            
         args = sorted(args)
+
         args.reverse()
         self.brush_sizes = list(args)
+
+
+class Brush:
+    
+    def __init__(self, image: Image.Image):
+        self.image = image.convert("L")
+    
+
+    def get_bitmap_for_size(self, size: int):
+        scaled_image = self.image.resize((size, size))
+        return scaled_image
--- Python/main.py
+++ Python/main.py
@@ -12,9 +12,8 @@ with Image.open(filename) as img:
     # labels, centers = slic_image.slic(superpixel_ratio=36, num_iterations=20, compactness=13)
     # slic_image.draw_splots(labels)
     layered_paint = LayeredPaintImage(img)
-    layered_paint.set_brush_sizes(5, 10, 20, 40)
-    #layered_paint.set_brush_sizes(80)
+    layered_paint.set_brush_sizes(6, 10, 20, 40)
     painted_image = layered_paint.paint()
-    painted_image.show()
+    #painted_image.show()
 
 painted_image.save(outputname)
